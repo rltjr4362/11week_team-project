38839b733902c386d21b20de612cb0eb5aaf34b6
MinJeong277 <kk1061106@naver.com>
2021-05-09 23:52:33 +0900

    'Crawling_2'

--- teamproject.py
+++ teamproject.py
@@ -1,9 +1,8 @@
 import pandas as pd
-import matplotlib as mpl
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-url = 'https://coronaboard.kr'
+url = 'https://coronaboard.kr/'
 
 driver = webdriver.Chrome('C:/Users/82102/Desktop/first_git_project/team-project/chromedriver.exe')
 driver.get(url) # url 불러오기
@@ -11,4 +10,29 @@ driver.implicitly_wait(3) # 3초 기다리기
 
 data = driver.page_source # 페이지 소스 가져오기
 
+soup = BeautifulSoup(data, 'html.parser')
 
+soup_select=soup.select('#country-table > div > div > table') #테이블 가져오기
+print(soup_select)
+df = pd.read_html(soup_select.prettify())[0]
+
+print(type(df))
+
+print(df)
+
+driver.quit()
+
+a=input('국가를 입력하세요: ')
+for i in range(75):
+    if a in df.iloc[i,1]:
+        nation=i
+        break
+list=[]
+c=''
+for i in range(2,5):
+    b=df.iloc[nation,i].split()[0]
+    c+=' '+b
+c=c.replace(',','').split()
+c = [int(i) for i in c]
+
+print(c)
